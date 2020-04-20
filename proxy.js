58a78b40b5343ec7b626710463ca10d649fefada
mac2 <mac2@example.com>
2020-04-20 14:01:01 +0900

    fix bug + support keep alive

--- proxy.js
+++ proxy.js
@@ -5,21 +5,29 @@ if (process.argv.length < 2) {
   return process.exit(1);
 }
 
-var httpProxy = require('http-proxy');
-var apiURL = 'https://www.bitmex.com';
-var port = process.argv[2];
+const http = require('http');
+const https = require('https');
+const httpProxy = require('http-proxy');
 
-var proxy = httpProxy.createProxyServer({});
+const apiURL = 'https://www.bitmex.com';
+const port = process.argv[2];
+const agent = new https.Agent({ keepAlive: true });
+const proxy = httpProxy.createProxyServer({ agent });
 
-var server = require('http').createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
   // API validates origin and referer to prevent certain types of csrf attacks, so delete them
   delete req.headers['origin'];
   delete req.headers['referer'];
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   req.url = '/api/v1' + req.url;
-  proxy.web(req, res, { target: apiURL });
+  proxy.web(req, res, {
+    changeOrigin: true,
+    target: apiURL
+  });
 });
+
+server.keepAliveTimeout = 90 * 1000
 
 server.listen(port);
 console.log('Started BitMEX proxy on port', port);
